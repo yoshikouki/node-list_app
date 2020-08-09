e7387177cc86804215b79a7e7451e3d9e942785f
yoshikouki <yoshikouki@gmail.com>
2020-08-09 12:46:02 +0900

    Top画面を作成。CSS と 画像ファイルをpublic ディレクトリ以下に配置

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.render('hello.ejs');
+app.use(express.static('public'));
+
+});
+
+app.get('/top', (req, res) => {
+  res.render('top.ejs');
 });
 
 app.listen(3000);
