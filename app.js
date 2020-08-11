8b2bb26771f8e8c516d4d8c3a99bc14243e04891
yoshikouki <yoshikouki@gmail.com>
2020-08-11 12:35:32 +0900

    データベース導入

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mysql = require('mysql')
 const app = express();
 
 app.use(express.static('public'));
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'progate',
+  password: 'password',
+  database: 'list_app'
+});
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
