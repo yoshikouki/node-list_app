edebcb6f3bea0b74f0336ed377fcb7c181c0a761
yoshikouki <yoshikouki@gmail.com>
2020-08-16 11:54:35 +0900

    リスト新規作成機能を実装

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
 const express = require('express');
-const mysql = require('mysql')
+const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
-const connection = mysql.createConnection({
+app.use(express.urlencoded({extended: false}));
+
+const connection_info = {
   host: 'localhost',
   user: 'progate',
   password: 'password',
   database: 'list_app'
-});
+};
+const connection = mysql.createPool(connection_info);
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
 });
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(results);
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'insert into items (name) values (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
 });
 
 app.listen(3000);
